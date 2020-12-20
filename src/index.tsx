f2bbf262387c6235d1c30e51fa562b5be49f41d0
thesafatem <thesafatem@gmail.com>
2020-12-21 02:31:32 +0600

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-  
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {ThemeProvider} from "./components/contexts/ThemeProvider";
-import {LanguageContext, LanguageProvider} from "./components/contexts/LanguageProvider";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider } from "./components/contexts/ThemeProvider";
+import { Provider } from "react-redux";
+import {
+  LanguageContext,
+  LanguageProvider,
+} from "./components/contexts/LanguageProvider";
+import store from "./store/index";
 
 ReactDOM.render(
+  <Provider store={store}>
     <ThemeProvider>
-        <LanguageProvider>
-                <App />
-            </LanguageProvider>
-    </ThemeProvider>,
-  document.getElementById('root')
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
