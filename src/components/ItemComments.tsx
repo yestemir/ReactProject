30387a8477b610cad12d247e19fb7fc3738e7148
thesafatem <thesafatem@gmail.com>
2020-12-21 21:48:28 +0600

    hide comment for unauthorized

--- src/components/ItemComments.tsx
+++ src/components/ItemComments.tsx
@@ -17,36 +17,52 @@ function ItemComments({ comments, setComments, curUser }: Props): ReactElement {
   const match = useRouteMatch<{ id: string }>();
   const [text, setText] = useState("");
   const product =
-      products.find((product) => product.id === Number(match.params.id)) ||
-      products[0];
+    products.find((product) => product.id === Number(match.params.id)) ||
+    products[0];
 
   const handleOnClick = () => {
     if (text.length && curUser) {
-      const newComment: Comment = { text, author: curUser, productId: product.id };
+      const newComment: Comment = {
+        text,
+        author: curUser,
+        productId: product.id,
+      };
       setComments([...comments, newComment]);
       setText("");
     }
   };
 
+  const showInput = (user: User | null) => {
+    if (user) {
+      return (
+        <div className="comments__top">
+          <Input
+            type="text"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            label="Leave new comment"
+            value={text}
+          />
+          <button onClick={handleOnClick}>Post</button>
+        </div>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
   return (
     <div style={{ padding: "200px 0" }}>
-      <div className="comments__top">
-        <Input
-          type="text"
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-          label="Leave new comment"
-          value={text}
-        />
-        <button onClick={handleOnClick}>Post</button>
-      </div>
-      {comments.filter((comment) => comment.productId === product.id).map((comment, index) => (
-        <div key={index}>
-          <div>Author: {comment.author.name}</div>
-          <div>{comment.text}</div>
-        </div>
-      ))}
+      {showInput(curUser)}
+      {comments
+        .filter((comment) => comment.productId === product.id)
+        .map((comment, index) => (
+          <div key={index}>
+            <div>Author: {comment.author.name}</div>
+            <div>{comment.text}</div>
+          </div>
+        ))}
     </div>
   );
 }
